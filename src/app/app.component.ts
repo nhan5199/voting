bbc645ce0e314f186a3e994c1442790938152b2a
Nhan.Trong <nhan,trong@fortressgb.com>
2025-05-10 22:47:34 +0700

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirebaseService } from './shared/services/firebase.service';
-import { FormsModule } from '@angular/forms';
+import {
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, CommonModule],
+  imports: [RouterOutlet, FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   item: any;
   items: any[] = [];
+  intervalId: any;
 
-  constructor(private firebaseService: FirebaseService) {}
+  formsInfo: FormGroup<any>;
 
-  ngOnInit() {
-    this.firebaseService.getData().subscribe((data) => (this.items = data));
+  constructor(
+    private firebaseService: FirebaseService,
+    private formBuilder: FormBuilder
+  ) {
+    this.formsInfo = this.formBuilder.group({
+      name: ['', Validators.required],
+      option: [0, Validators.required],
+    });
   }
 
-  addItem() {
-    if (this.item.trim()) {
-      this.firebaseService
-        .addData({ name: this.item, lastName: this.item, test: this.item })
-        .then(() => (this.item = ''));
+  ngOnInit() {
+    this.intervalId = setInterval(() => {
+      this.firebaseService.getData().subscribe((data) => (this.items = data));
+    }, 60000);
+  }
+
+  ngOnDestroy(): void {
+    // Clear the interval when the component is destroyed to prevent memory leaks
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
     }
   }
 
-  deleteItem(id: string) {
-    // this.firebaseService.deleteData(id);
+  addItem() {
+    if (this.formsInfo.valid) {
+      this.firebaseService
+        .addData({
+          name: this.formsInfo.value?.name,
+          option: this.formsInfo.value?.option,
+        })
+        .then(() => console.log('data: ', this.formsInfo.value));
+    }
   }
+
+  // deleteItem(id: string) {
+  //   this.firebaseService.deleteData(id);
+  // }
 }
