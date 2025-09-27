dcb0b64cf5893839198df6fa7073aa89687ccc7e
Nhan <nhan.trong@fortressgb.com>
2025-09-27 14:00:39 +0700

    update voting result

--- src/app/result-boss/result-boss.component.ts
+++ src/app/result-boss/result-boss.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FirebaseService } from '../shared/services/firebase.service';
 import { firstValueFrom } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-voting-result',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './result-boss.component.html',
   styleUrl: './result-boss.component.scss',
 })
@@ -16,33 +17,46 @@ export class ResultBossComponent implements OnInit, OnDestroy {
   intervalId: any;
   async ngOnInit() {
     this.items = [
-      {
-        name: 'Tiết mục 1',
-        option: 1,
-        count: 0,
-      },
-
-      {
-        name: 'Tiết mục 2',
-        option: 2,
-        count: 0,
-      }
-      ,{
-        name: 'Tiết mục 3',
-        option: 3,
-        count: 0,
-      },
-
-      {
-        name: 'Tiết mục 4',
-        option: 4,
-        count: 0,
-      }
-    ];
+    {
+      name: 'Tiết mục 1',
+      option: 1,
+      count: 0,
+      imgUrl1:"/images/boss/voting/1.jpg",
+      imgUrl2:"/images/boss/voting/1.jpg"
+    },
+    {
+      name: 'Tiết mục 2',
+      option: 2,
+      count: 0,
+      imgUrl1:"",
+      imgUrl2:""
+    },
+    {
+      name: 'Tiết mục 3',
+      option: 3,
+      count: 0,
+      imgUrl1:"",
+      imgUrl2:""
+    },
+    {
+      name: 'Tiết mục 4',
+      option: 4,
+      count: 0,
+      imgUrl1:"",
+      imgUrl2:""
+    },
+    {
+      name: 'Tiết mục 5',
+      option: 5,
+      count: 0,
+      imgUrl1:"",
+      imgUrl2:""
+    }
+  ];
     this.getData();
     this.intervalId = setInterval(() => {
       this.getData();
-    }, 20000); // 30 seconds
+    }, 5000); // 15 seconds
   }
 
   async getData() {
